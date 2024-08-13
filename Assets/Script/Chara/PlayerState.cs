4ddff2287706d28a43771a94959cfa971720fbba
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2024-08-14 01:43:08 +0900

    プレイヤーの状態毎の処理を行う基底クラスの実装
    [PlayerState.cs]
    ・プレイヤーの状態毎の処理を行う基底クラス
    ・コンポーネントを取得する必要があるため、現状EnterメソッドでPlayerMoveを引数にしている
    ・プレイヤーの状態は、後に実装するPlayerMove.cs内で切り替える
    ・MonoBehaviourを継承しないためパラメータは直接いじる必要あり

---
+++ Assets/Script/Chara/PlayerState.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief 	プレイヤーの状態毎の処理を行う基底クラス
+ * 
+ *  @memo   ・プレイヤーの状態は、後に実装するPlayerMove.cs内で列挙型(CharaCondition、PlayerCondition)を使用して切り替える
+ *  
+ *  ========================================================================================================
+ *  
+ *          MonoBehaviourを継承しないためパラメータは直接いじってください！！！！！！！！！
+ *          
+ *  ========================================================================================================       
+*/
+public abstract class PlayerState
+{
+    //===============================================
+    //          マトリョーシカの揺れ
+    //===============================================
+
+    protected float tiltVelocity = 0.0f;              // 傾きの速度
+    protected const float tiltAmount = 45.0f;         // 回転角の最大値
+    protected const float damping = 0.1f;             // 減衰係数(どのくらいずつ回転角度を減らしていくか)
+    protected const int maxSwimg = 3;                 // 何回揺れるか 
+    protected const　int swingCount = 0;              // 揺れの回数をカウント
+    protected const　float angleSwingZone = 1.0f;     // 揺れた判定内かどうか
+    protected bool isInDeadZone = false;              // 揺れのデッドゾーン内にいるかどうか
+
+    /**
+     * @brief 	この状態に入るときに行う関数
+     * @paraam  PlayerMove _playerMove  
+     * 
+     * memo    RigidBody2Dやその他コンポーネントを取得するためのみに使用する
+    */
+    public abstract void Enter(PlayerMove _playerMove);
+
+    /**
+     * @brief 	この状態から出るときに行う関数
+    */
+    public abstract void Exit();
+
+    /**
+     * @brief 	更新処理t
+    */
+    public abstract void Update();
+
+    /**
+     * @brief 	当たった時の処理
+     *  @param  Collision _collision    当たったオブジェクト
+    */
+    public abstract void CollisionEnter(Collision _collision);
+
+    ///**
+    // * @brief 	当たらなくなった時の処理
+    // *  @param  Collision _collision    当たったオブジェクト
+    //*/
+    //public abstract void CollisionExit(Collision _collision);
+}
