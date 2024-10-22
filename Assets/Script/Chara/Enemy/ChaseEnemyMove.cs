a9f552839f2fd824f7d135aaedeece503b9a0cb9
Klbs-delicious <takahashi.kaho.30677@gmail.com>
2024-10-22 20:43:07 +0900

    後ろから追いかけてくる敵の移動スクリプトの追加
    ・これするぞ～というコメントのみ記述

---
+++ Assets/Script/Chara/Enemy/ChaseEnemyMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @brief   後ろから追いかけてくる敵
+ * 
+ * @memo    ・ マトリョーシカの後ろに来たとき攻撃アニメーションを実行する
+ *          ・ 攻撃中は移動アニメーションレイヤーを一時停止する
+ *          ・ 攻撃アニメーションが終了した時点で(又はアニメーションがマトリョーシカに当たった時)プレイヤーはゲームオーバーになる
+ */
+public class ChaseEnemyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
